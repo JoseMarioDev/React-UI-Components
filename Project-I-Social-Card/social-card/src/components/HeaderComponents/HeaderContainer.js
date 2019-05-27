73a2743bc533fcfd3e2a3dee2bc474f0255b16e7
Jose Montero Jr <josemariodev@gmail.com>
2019-05-27 12:06:17 -0500

    header components styled

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Header.scss';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
   return (
     <div className='headerContainer'>
-      <ImageThumbnail />
-      <HeaderTitle />
+      <div className='headerLeft'>
+        <ImageThumbnail />
+      </div>
+      <div className='headerRight'>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -4,7 +4,10 @@ import './Header.scss';
 const ImageThumbnail = () => {
   return (
     <div className='imgContainer'>
-      <img src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png' />
+      <img
+        src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png'
+        alt='red logo'
+      />
     </div>
   );
 };
