385fcb71f33bca069c1886061b8dda38d2007620
Jose Montero Jr <josemariodev@gmail.com>
2019-05-27 12:59:51 -0500

    cardCompents updated, styled

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Card.scss';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => {
   return (
     <div className='cardContainer'>
       <CardBanner />
+      <CardContent />
     </div>
   );
 };
