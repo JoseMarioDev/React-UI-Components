5e811c29c6acb9125440d1beea572a03c7e6ce1b
Jose Montero Jr <josemariodev@gmail.com>
2019-05-27 10:56:15 -0500

    headerTitle and imageThumbnail components updated

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.scss';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+  return (
+    <div className='headerContainer'>
+      <ImageThumbnail />
+      <HeaderTitle />
+    </div>
+  );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.scss';
+
+const HeaderTitle = () => {
+  return (
+    <div className='headerTitle'>
+      <h1>Lambda School</h1>
+      <p className='lightText'>@LambdaSchool * 26 jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
