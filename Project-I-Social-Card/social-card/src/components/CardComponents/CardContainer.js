c3708069db0408c537caeb9d803a7e51858143de
Jose Montero Jr <josemariodev@gmail.com>
2019-05-27 12:49:07 -0500

    cardBanner added

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.scss';
+import CardBanner from './CardBanner';
+
+const CardContainer = () => {
+  return (
+    <div className='cardContainer'>
+      <CardBanner />
+    </div>
+  );
+};
+
+export default CardContainer;
