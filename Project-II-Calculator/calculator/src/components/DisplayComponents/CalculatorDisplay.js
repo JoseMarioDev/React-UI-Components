a620ffedab0f239bf858e1268b436f43b27bc3ab
Jose Montero Jr <josemariodev@gmail.com>
2019-05-27 21:39:29 -0500

    calc updated

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
-import './Display.css';
+import './Display.scss';
+
+const CalculatorDisplay = props => {
+  const { display } = props;
+  return (
+    <div className='calculatorDisplay'>
+      <h1>{display.total}</h1>
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
