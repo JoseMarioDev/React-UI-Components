424a3586a4177959f7ae574ade1ecc4e2d769b31
Jose Montero Jr <josemariodev@gmail.com>
2019-05-27 21:28:09 -0500

    numberButtons component updated

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,62 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+  const { number } = props;
+  return (
+    <div className='numberButton'>
+      <div className='clearRow'>
+        <button>{number.divide}</button>
+      </div>
+      <div className='row1'>
+        <div className='row1-1'>
+          <button>{number.seven}</button>
+        </div>
+        <div className='row1-2'>
+          <button>{number.eight}</button>
+        </div>
+        <div className='row1-3'>
+          <button>{number.nine}</button>
+        </div>
+        <div className='row1-sign'>
+          <button>{number.times}</button>
+        </div>
+      </div>
+
+      <div className='row2'>
+        <div className='row2-1'>
+          <button>{number.seven}</button>
+        </div>
+        <div className='row2-2'>
+          <button>{number.eight}</button>
+        </div>
+        <div className='row2-3'>
+          <button>{number.nine}</button>
+        </div>
+        <div className='row2-sign'>
+          <button>{number.minus}</button>
+        </div>
+      </div>
+
+      <div className='row3'>
+        <div className='row3-1'>
+          <button>{number.seven}</button>
+        </div>
+        <div className='row3-2'>
+          <button>{number.eight}</button>
+        </div>
+        <div className='row3-3'>
+          <button>{number.nine}</button>
+        </div>
+        <div className='row3-sign'>
+          <button>{number.plus}</button>
+        </div>
+      </div>
+      <div className='zero-row'>
+        <button>{number.equal}</button>
+      </div>
+    </div>
+  );
+};
+
+export default NumberButton;
